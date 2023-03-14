eb1dbeed64851d87844288db7a317f8f68b4e4c3
pankaj desai <desaipankaj740@gmail.com>
2023-03-14 21:40:43 +0530

    header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='text-3xl text-red-400 items-center font-bold border-b-2 p-3 border-gray-400'>
+    <span>Filmy<span className='text-white'>Rise</span> </span> 
+    </div>
+  )
+}
+
+export default Header
