5769aef617b1de6beb856fa9dd2e761f417bb44e
pankaj desai <desaipankaj740@gmail.com>
2023-03-15 17:56:57 +0530

    button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import AddIcon from '@mui/icons-material/Add';
+import { Button } from '@mui/material';
 const Header = () => {
   return (
-    <div className='text-3xl text-red-400 items-center font-bold border-b-2 p-3 border-gray-400'>
+    <div className='text-3xl flex justify-between text-red-400 items-center font-bold border-b-2 p-3 border-gray-400'>
     <span>Filmy<span className='text-white'>Rise</span> </span> 
+    <h1 className='text-lg flex items-center cursor-pointer'>
+      <Button>< AddIcon className='m-0.5 ' color='secondary'/>
+      <span  className='text-white'> Add New</span>
+       </Button>
+       </h1>
     </div>
   )
 }
