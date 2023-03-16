e5412bd92d0e99f66363e979fed6237c11bdc8b4
pankaj desai <desaipankaj740@gmail.com>
2023-03-16 22:08:48 +0530

    Feat: dynamic fetching

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,89 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect} from 'react';
+import { Audio } from 'react-loader-spinner';
 import ReactStars from 'react-stars';
+import { getDocs } from 'firebase/firestore';
+import { moviesRef } from '../firebase/firebase';
+
 
 const Cards = () => {
 
-  const [data, setData] = useState([
+  const [data, setData] = useState([]);
+  const [loading, setLoading]=useState(false);
 
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
+  useEffect(() => {
+   async function getData() {
+    setLoading(true);
+    const _data = await getDocs(moviesRef);
+    console.log(_data);
+    _data.forEach((doc => {
+      setData((prev) => [...prev,doc.data()])
+    }))
 
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    },
-    {
-      name: "Vikram",
-      rating: 5,
-      year: 2022,
-      img: "https://wallpaperaccess.com/full/8242549.jpg"
-    }
-  ]);
+    setLoading(false);
+   }
+   getData();
+  }, [])
+  
 
 
   return (
-    <div className='flex flex-wrap justify-between p3 mt-3'>
-      {
+    <div className='flex flex-wrap justify-between  p3 mt-3'>
+      { 
+      loading ? <div className='w-full flex justify-center items-center h-96'><Audio height={40} color="white"/> </div>:
         data.map((e, i) => {
           return (
             <div key={i} className='cards text-sm font-bold shadow-lg p-2 cursor-pointer
          hover:-translate-y-3 mt-6 transition-all duration-700'>
-              <img className='h-72' src={e.img} alt="" />
+              <img className='h-96 w-auto' src={e.image} alt="" />
               <h1> <span className='text-blue-200'>Name:</span>{e.name}</h1>
               <h1 className='flex items-center mr-1'><span className='text-blue-200'>Rating: </span>
                 < ReactStars
                   size={20}
                   half={true}
-                  value={e.rating}
+                  value={5}
                   edit={false}
                 />
               </h1>
